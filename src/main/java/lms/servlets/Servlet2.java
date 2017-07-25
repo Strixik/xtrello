93b864cb7d252b45473cd393c83028fd3a7f7944
Strixik <strixik1985@gmail.com>
2017-07-25 17:16:27 +0300

    Strix
    v.0.0.1

---
+++ src/main/java/lms/servlets/Servlet2.java
@@ -0,0 +1,64 @@
+
+package lms.servlets;
+import lms.models.User;
+import lms.view.PageParts;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "Second", urlPatterns = {"/reg"})
+public class Servlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws
+            ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        String ulogin = new String(request.getParameter("newLogin").getBytes("iso-8859-1"), "UTF-8");
+        String upassword = new String(request.getParameter("newPassword").getBytes("iso-8859-1"), "UTF-8");
+        String uemail = new String(request.getParameter("newEmail").getBytes("iso-8859-1"), "UTF-8");
+        User user = new User();
+        user.setLogin(ulogin);
+        user.setPassword(upassword);
+        user.setEmail(uemail);
+        if (user.findByLogin(ulogin)){
+
+            out.write(PageParts.getTag("h3","This login is busy!",""));
+            out.write(PageParts.getTag("a","Back","href=\"/registration\""));
+        }
+        else {
+            if (user.getLogin() == null || user.getLogin().equals("") || user.getPassword() == null || user.getPassword().equals("")|| user.getEmail() == null || user.getEmail().equals("")){
+                out.write(PageParts.getTag("h3","Empty fields!",""));
+                out.write(PageParts.getTag("a","Back","href=\"/registration\""));
+            }
+            else {
+            user.insert(user.getLogin(),user.getPassword(), user.getEmail());
+            out.write(PageParts.getTag("h3","Created a new user!",""));
+            out.write(PageParts.getTag("a","Back","href=\"/\""));
+            HttpSession session = request.getSession();}
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws
+            ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        try {
+            System.out.println("servlet2");
+
+
+
+
+        } finally {
+            out.close();
+        }
+    }
+}
---
+++ src/main/java/lms/servlets/Servlet3.java
@@ -0,0 +1,44 @@
+package lms.servlets;
+
+import lms.models.Sticker;
+import lms.view.PageParts;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static lms.servlets.ServletBoards.sticker;
+
+@WebServlet(name = "Servlet3", urlPatterns = {"/note"})
+        public class Servlet3 extends HttpServlet {
+            protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                PrintWriter out = response.getWriter();
+                HttpSession session = request.getSession();
+                session.getAttribute("login");
+                session.getAttribute("id");
+                String text = new String(request.getParameter("inputtext").getBytes("iso-8859-1"), "UTF-8");
+                String name = new String(request.getParameter("name").getBytes("iso-8859-1"), "UTF-8");
+                if (text != null || text.equals("")){
+                    sticker.setNote(text);
+                    sticker.insertNote(Integer.parseInt(session.getAttribute("id").toString()),name,sticker.getNote());
+                    response.sendRedirect("/");
+                }
+                else {
+                    response.sendRedirect("/");
+                }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        session.getAttribute("login");
+        session.getAttribute("id");
+        System.out.println("servlet 3");
+      }
+}
